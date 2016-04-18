92d3aec5c52f1307d5c58353f719b541813df596
KeyboardFire <andy@keyboardfire.com>
2016-04-18 11:39:41 -0500

    Add vowels to UI

--- src/builder.rb
+++ src/builder.rb
@@ -1,14 +1,23 @@
 #!/usr/bin/env ruby
 
-data = '|Bilabial|Labiodental|Dental|Alveolar|Postalveolar|Palatal|Velar|Uvular|Pharyngeal|Glottal
-Plosive            |p|b|_|_|_|_|t|d|_|_|c|ɟ|k|g|q|ɢ|_|#|ʔ|#
-Nasal              |_|m|_|ɱ|_|_|_|n|_|_|_|ɲ|_|ŋ|_|ɴ|#|#|#|#
-Trill              |_|ʙ|_|_|_|_|_|r|_|_|_|_|#|#|_|ʀ|_|_|#|#
-Tap/Flap           |_|_|_|ⱱ|_|_|_|ɾ|_|_|_|_|#|#|_|_|_|_|#|#
-Fricative          |ɸ|β|f|v|θ|ð|s|z|ʃ|ʒ|ç|ʝ|x|ɣ|χ|ʁ|ħ|ʕ|h|ɦ
-Lateral fricative  |#|#|#|#|_|_|ɬ|ɮ|_|_|_|_|_|_|_|_|#|#|#|#
-Approximant        |_|_|_|ʋ|_|_|_|ɹ|_|_|_|j|_|ɰ|_|_|_|_|#|#
-Lateral approximant|#|#|#|#|_|_|_|l|_|_|_|ʎ|_|ʟ|_|_|#|#|#|#'
+data = '1|2Bilabial|2Labiodental|2Dental|2Alveolar|2Postalveolar|2Palatal|2Velar|2Uvular|2Pharyngeal|2Glottal
+1Plosive            |p|b|_|_|_|_|t|d|_|_|c|ɟ|k|g|q|ɢ|_|#|ʔ|#
+1Nasal              |_|m|_|ɱ|_|_|_|n|_|_|_|ɲ|_|ŋ|_|ɴ|#|#|#|#
+1Trill              |_|ʙ|_|_|_|_|_|r|_|_|_|_|#|#|_|ʀ|_|_|#|#
+1Tap/Flap           |_|_|_|ⱱ|_|_|_|ɾ|_|_|_|_|#|#|_|_|_|_|#|#
+1Fricative          |ɸ|β|f|v|θ|ð|s|z|ʃ|ʒ|ç|ʝ|x|ɣ|χ|ʁ|ħ|ʕ|h|ɦ
+1Lateral fricative  |#|#|#|#|_|_|ɬ|ɮ|_|_|_|_|_|_|_|_|#|#|#|#
+1Approximant        |_|_|_|ʋ|_|_|_|ɹ|_|_|_|j|_|ɰ|_|_|_|_|#|#
+1Lateral approximant|#|#|#|#|_|_|_|l|_|_|_|ʎ|_|ʟ|_|_|#|#|#|#
+1|2Front|2Central|2Back
+1Close|i|y|ɨ|ʉ|ɯ|u
+1Near-close|ɪ|ʏ|_|_|_|ʊ
+1Close-mid|e|ø|ɘ|ɵ|ɤ|o
+1Mid|_|_|ə
+1Open-mid|ɛ|œ|ɜ|ɞ|ʌ|ɔ
+1Near-open|æ|_|ɐ
+1Open|a|ɶ|_|_|ɑ|ɒ
+'
 
 puts "
 <interface>
@@ -22,12 +31,17 @@ puts "
                 <property name='visible'>True</property>"
 
 data.split("\n").each_with_index do |line, i|
+    xpos = 0
     line.split(?|).each_with_index do |cell, j|
-        next if cell.strip.empty?
         grey = cell == ?#
-        is_button = !grey && cell != ?_
-        cell = '' unless is_button
-        is_button &= i != 0 && j != 0
+        empty = cell == ?_
+        cell = '' if grey || empty
+
+        label_width = if cell =~ /^\d/; cell.match(/\d/)[0].to_i; end
+        cell.sub! /^\d/, ''
+
+        is_button = !(grey || empty || label_width)
+
         puts "
                 <child>
                     <object id='cell_#{i}_#{j}' class='#{is_button ? 'GtkButton' : 'GtkLabel'}'>
@@ -38,10 +52,12 @@ data.split("\n").each_with_index do |line, i|
                     </object>
                     <packing>
                         <property name='top-attach'>#{i}</property>
-                        <property name='left-attach'>#{i == 0 ? j*2-1 : j}</property>
-                        #{i == 0 ? "<property name='width'>2</property>" : ''}
+                        <property name='left-attach'>#{xpos}</property>
+                        #{label_width ? "<property name='width'>#{label_width}</property>" : ''}
                     </packing>
                 </child>"
+
+        xpos += label_width || 1
     end
 end
 
@@ -51,7 +67,7 @@ puts "
                         <property name='visible'>True</property>
                     </object>
                     <packing>
-                        <property name='top-attach'>9</property>
+                        <property name='top-attach'>#{data.count "\n"}</property>
                         <property name='left-attach'>0</property>
                         <property name='width'>21</property>
                     </packing>
