bde3e93b77e4b5b258d9ebbc173ca6a9d0bce0cd
KeyboardFire <andy@keyboardfire.com>
2016-04-18 09:12:31 -0500

    Add more digraphs

--- src/digraphs.rb
+++ src/digraphs.rb
@@ -1,18 +1,31 @@
 #!/usr/bin/env ruby
 
 data = '
+    { symbols that can be found in the clickable table as well }
     j-ɟ GGɢ ??ʔ
     m,ɱ n,ŋ ŋ,ɲ NNɴ
     BBʙ RRʀ
+    rOɾ r0ɾ
     PHɸ BHβ THθ DHð SHʃ ZHʒ c,ç j,ʝ x,ɣ ɣ,χ XXχ ʀ/ʁ h-ħ ʔ/ʕ ?/ʕ h,ɦ
     l-ɬ LZɮ
+    vOʋ v0ʋ r/ɹ w,ɰ w|ɰ
+    y/ʎ LLʟ
+
+    { retroflexes }
+    t)ʈ d)ɖ n)ɳ r)ɽ ɾ)ɽ s)ʂ z)ʐ ɹ)ɻ l)ɭ
+
+    { non-pulmonics }
+    o.ʘ |=ǂ ||ǁ
+    b(ɓ d(ɗ j(ʄ ɟ(ʄ g(ɠ ɢ(ʛ
+    ''ʼ
+
+    { misc. symbols }
+    w/ʍ h/ɥ HHʜ ʕ-ʢ ?-ʡ ʔ-ʡ SJɕ ZJʑ RLɺ LRɺ ɾlɺ lɾɺ ʃxɧ xʃɧ
 '
-data = data.scan(/\S/).each_slice(3).flat_map{|digraph|
-    if digraph.join =~ /^[!-~]{2}/
-        [digraph]
-    else
-        [digraph, digraph.reverse]
-    end
+data = data.gsub(/\{[^}]*\}/, '').scan(/\S/).each_slice(3).flat_map{|digraph|
+    digraph.join =~ /^[!-~]{2}/ ? [digraph] : [digraph, digraph.reverse]
+}.flat_map{|digraph|
+    [digraph, digraph.join.sub(/[a-z]/){|x|x.upcase}.chars].uniq
 }
 
 puts data.map{|digraph|
