598b2f6b9eb7a6959b447663b190098253b89507
KeyboardFire <andy@keyboardfire.com>
2016-04-22 20:28:18 -0500

    Oh I needed sponge...

--- go.sh
+++ go.sh
@@ -8,4 +8,4 @@ make
 # web
 tools/digraphs.rb > web/digraphs.txt
 tools/builder.rb html web/digraphs.txt > web/builder.html
-tools/copyusage.rb README.md web/index.html > web/index.html
+tools/copyusage.rb README.md web/index.html | sponge web/index.html
